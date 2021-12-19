af92868b1b6ed908d0cf73c0e00def97f8c32048
Miller Wesselhoff <millerwesselhoff@gmail.com>
2021-12-19 13:51:39 -0600

    created case normalizing function and function that plays a round and outputs results of round

--- script.js
+++ script.js
@@ -15,4 +15,30 @@ function computerPlay() {
     return choice;
 }
 
-console.log(computerPlay());
+function normalizeCases(str) {
+    let firstLetter = str.charAt(0).toUpperCase();
+    let lastLetters = str.slice(1).toLowerCase();
+    return firstLetter+lastLetters;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result = ""
+    let playerNorm = normalizeCases(playerSelection);
+    if(playerNorm === computerSelection) {
+        result = `It's a tie! You both picked ${playerNorm}.`
+    } else if (playerNorm === "Rock" && computerSelection === "Scissors") {
+        result = `You win! Rock beats Scissors.`
+    } else if (playerNorm === "Rock" && computerSelection === "Paper") {
+        result = `You lose! Paper beats Rock.`
+    } else if (playerNorm === "Paper" && computerSelection === "Rock") {
+        result = `You win! Paper beats Rock.`
+    } else if (playerNorm === "Paper" && computerSelection === "Scissors") {
+        result = `You lose! Scissors beats Paper.`
+    } else if (playerNorm === "Scissors" && computerSelection === "Paper") {
+        result = `You win! Scissors beats Paper.`
+    } else if (playerNorm === "Scissors" && computerSelection === "Rock") {
+        result = `You lose! Rock beats Scissors.`
+    }
+
+    return result;
+}
